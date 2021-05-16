38c235018e524b22535a60a8a08a172aadbd8479
tinhthanh <k40cntt@gmail.com>
2021-05-16 14:14:09 +0700

    date format

--- svg-common-icons/src/app/app.component.ts
+++ svg-common-icons/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +6,50 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'svg-common-icons';
+
+  constructor() {
+const f = new Date();
+f.setDate(f.getDate());
+var days = this.formatTime(f);
+
+console.log(days)
+
+const f2 = new Date();
+f2.setDate(f2.getDate() -1 );
+var days2 = this.formatTime(f2);
+
+console.log(days2)
+
+
+const f3 = new Date();
+f3.setDate(f3.getDate() - 2 );
+var days3 = this.formatTime(f3);
+
+console.log(days3)
+
+
+
+
+
+const f4 = new Date();
+f4.setDate(f4.getDate() - 8 );
+var days4 = this.formatTime(f4);
+
+console.log(days4)
+  }
+
+ formatTime(time: Date) {
+  const fn = (value: Number) => String(value).padStart(2, '0');
+  const type = Math.abs(Date.now() - time.getTime()) / (1000 * 3600 * 24) ;
+  switch (type) {
+      case 0:
+          return `Today - ${fn(time.getHours())}:${fn(time.getMinutes())}` ;
+      case 1:
+          return `Yesterday - ${fn(time.getHours())}:${fn(time.getMinutes())}`
+      case 2 || 3 || 4 || 5 || 6 || 7:    
+           return `${type +  1} days ago`
+      default:
+           return  `${fn(time.getDay())} ${time.toLocaleString("defalt", { month : 'long'})} ${time.getFullYear().toString().substr(-2)}`;
+    }
+}
 }
